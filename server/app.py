cce6712ef4b84bd1252d0c31af927487198fec84
Patricia Mwangi <patricia.mwangi@student.moringaschool.com>
2024-07-02 02:01:51 +0300

    flask-sqalchemy

--- server/app.py
+++ server/app.py
@@ -20,6 +20,27 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake= Earthquake.query.filter(Earthquake.id==id).first()
+    if earthquake: 
+        body=earthquake.to_dict()
+        status=200
+    else:
+        body={'message':f'Earthquake {id} not found'}
+        status=404
+    return make_response(body,status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def min_magnitude(magnitude):
+    magnitudes=[]
+    for m in Earthquake.query.filter(magnitude <= Earthquake.magnitude):
+        magnitudes.append(m.to_dict())
+    body={
+            'count':len(magnitudes),
+            'quakes':magnitudes
+        }
+    return make_response(body,200)
 # Add views here
 
 
